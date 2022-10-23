3b410dc3b7bff18c81553cc645e77589645e5f90
romstroller <romstroller1@gmail.com>
2022-10-23 14:15:43 +1000

    clipShuff and pathSearch minor refactors

--- clipShuff.py
+++ clipShuff.py
@@ -1,48 +1,55 @@
-
 import random
 import pandas as pd
 
+
 def shuffleClipList():
-    
-    if itemsDF.shape[0] <= 2: return print(nan)
     # pd clipboard method takes first item as colName; reinsert as row
-    el = itemsDF.columns[0]
+    _el = itemsDF.columns[ 0 ]
     itemsDF.columns = [ "items" ]
-    itemsDF.loc[-1] = el
-    itemsDF.sort_index(inplace=True)
-    print( f"{itemsDF.shape[0]} ITEMS FROM CLIPBOARD:\n", itemsDF, "\n")
-    itemsDF_shuff = itemsDF.sample(frac=1).reset_index(drop=True)
+    itemsDF.loc[ -1 ] = _el
+    itemsDF.sort_index( inplace=True )
+    
+    print( f"{itemsDF.shape[ 0 ]} ITEMS FROM CLIPBOARD:\n", itemsDF, "\n" )
+    itemsDF_sh = itemsDF.sample( frac=1 ).reset_index( drop=True )
     
     # rand re-insert 1st row if unmoved, and even callously dismissive
-    if itemsDF_shuff.loc[0].values == itemsDF.loc[0].values:
+    if itemsDF_sh.loc[ 0 ].values == itemsDF.loc[ -1 ].values:
         # get rand loc in df range (not at start or end)
-        def randLoc(): return random.randrange(itemsDF_shuff.shape[0]-1)
+        def randLoc(): return random.randrange( 1, itemsDF_sh.shape[ 0 ] - 1 )
+        
         rLoc = randLoc()
-        while rLoc == 0 or rLoc > itemsDF_shuff.shape[0]-1: rLoc = randLoc()
+        while rLoc == 0 or rLoc > itemsDF_sh.shape[ 0 ] - 1: rLoc = randLoc()
         
         # split at loc, and join around row
-        itemsDF_shuff = pd.concat( [
-            itemsDF_shuff.loc[1:rLoc],
-            itemsDF_shuff.loc[0:0],
-            itemsDF_shuff.loc[rLoc+1:],
-            ], ignore_index=True)
+        itemsDF_sh = pd.concat( [
+            itemsDF_sh.loc[ 1:rLoc ],
+            itemsDF_sh.loc[ 0:0 ],
+            itemsDF_sh.loc[ rLoc + 1: ],
+            ], ignore_index=True )
     
-    return [ v for v in itemsDF_shuff["items"].values ]
+    return [ v for v in itemsDF_sh[ "items" ].values ]
 
-errs = ( StopIteration, pd.errors.EmptyDataError, pd.errors.ParserError ) 
+
+ers = (
+    AssertionError,
+    StopIteration,
+    pd.errors.EmptyDataError,
+    pd.errors.ParserError)
+    
+msg = "Clipped not single-column list of 4+ items."
 
 while True:
-    try: itemsDF = pd.read_clipboard()
-    except errs: pass
-    nan = "Clipped not single-column list of 4+ items."
-    try: sh = itemsDF.shape[0]
-    except NameError: print(nan)
+    try:
+        itemsDF = pd.read_clipboard()
+        assert itemsDF.shape[ 0 ] > 2  # len 4+
+    except ers: print( msg )
     else:
         shuff = shuffleClipList()
-        pd.io.clipboard.copy("\n".join(shuff))
-        print( "SHUFFLED, COPIED TO CLIPBOARD:\n")
+        pd.io.clipboard.copy( "\n".join( shuff ) )
+        print( "SHUFFLED, COPIED TO CLIPBOARD:\n" )
         for el in shuff: print( el )
-    input( "\nenter to reshuffle\n" )
-    
-# EXTEND: allow multi-column clip shuffle
+        
+    input( "\nenter to shuffle again\n" )
 
+# EXTEND:
+#   allow multi-column clip shuffle
--- pathSearch.py
+++ pathSearch.py
@@ -1,4 +1,3 @@
-
 import os
 import re
 import subprocess
@@ -6,109 +5,105 @@ import subprocess
 
 def getInp(): return input( "\nEnter search terms or '~' to break: " )
 
-def getPattern( _inp):
-    inSplit = [ c for c in re.split("[-_\.\s,]+", _inp.lower()) if len(c)>0 ]
-    if not inSplit: return _inp  # in case a file is named only with seps ?
-    
-    patt = inSplit.pop(0)
-    # prefix middle segments with "0+ any"
-    while len(inSplit)>1:
-        if len(p:= inSplit.pop(0) )>0: patt+= f".*{p}"  # alt: [^\\]*
-    
-    if inSplit: patt+= r".*" + inSplit.pop(0)  # alt: [^\\]*
-    
-    return re.compile(patt)
 
-def getWalkDct(): return { 
-    path : { "folds" : sDirs, "files" : files }
-    for path, sDirs, files in os.walk(".") }
+def getPattern( _inp ):
+    inSpl = [ c for c in re.split( "[-_.\s,]+", _inp.lower() ) if len( c ) > 0 ]
+    if not inSpl: return _inp  # in case a file is named only with seps
+    
+    _pat = inSpl.pop( 0 )
+    # add subsequent non-empty segments, sep'd by with re "0+ any"
+    while len( inSpl ) >= 1:
+        if len( p := inSpl.pop( 0 ) ) > 0: _pat += f".*{p}"  # alt: [^\\]*
+    
+    return re.compile( _pat )
+
+
+def getWalkDct(): return {
+    path: { "folds": sDirs, "files": files }
+    for path, sDirs, files in os.walk( "." ) }
+
 
 def getAction( _rslts ):
-    
-    def getActRef(): return input( f"\n"
-        f"[ g 4 ] - go to result 4 in explorer\n"
-        f"[ o 7 ] - run/open result 7\n"
-        f"[ p 2 ] - print full path for result 2\n"
-        f"[  s  ] - run a new search\n"
-        f"[  ~  ] - exit pathSearch utility\n\n" )
+    def getActRef(): return input(
+        f"\n[ g 4 ] - go to result 4 in explorer"
+        f"\n[ o 7 ] - run/open result 7"
+        f"\n[ p 2 ] - print full path for result 2"
+        f"\n[  s  ] - run a new search"
+        f"\n[  ~  ] - exit pathSearch utility\n\n" )
     
     def getRefPath( _inp ):
-        try: num = int(_inp[1:])
+        try: _num = int( _inp[ 1: ] )
         except ValueError: pass
         else:
-            for rNum, fil, pth in _rslts:
-                if num == rNum: return f"{pth}\\{fil}" if fil else pth
+            for _rNum, _fil, _pth in _rslts:
+                if _num == _rNum: return f"{_pth}\\{_fil}" if _fil else _pth
+        
         print( f"Result reference missing or invalid: [ {_inp} ]" )
     
     def goTo( _inp ):
         rPth = getRefPath( _inp )
-        if rPth: subprocess.Popen(r'explorer /select,' + rPth)
-        
-    def start( _inp ): 
+        if rPth: subprocess.Popen( r'explorer /select,' + rPth )
+    
+    def start( _inp ):
         rPth = getRefPath( _inp )
-        if rPth: os.startfile(rPth)
-        
+        if rPth: os.startfile( rPth )
+    
     def getPath( _inp ):
         rPth = getRefPath( _inp )
-        if rPth: print( os.getcwd() + rPth[1:] )
+        if rPth: print( os.getcwd() + rPth[ 1: ] )
     
     while True:
         inp = getActRef()
         if not inp: continue
-        match inp[0]:
-            case 'g' | 'G': goTo(inp)
-            case 'o' | 'O': start(inp)
-            case 'p' | 'P': getPath(inp)
+        match inp[ 0 ]:
+            case 'g' | 'G': goTo( inp )
+            case 'o' | 'O': start( inp )
+            case 'p' | 'P': getPath( inp )
             case 's' | 'S': return getInp()
             case '~': return inp
-            case _: print(f'Not action at first position: [ {inp[0]} ]')
-
+            case ___: print( f'Not action at first position: [ {inp[ 0 ]} ]' )
 
 
 # MAIN
 
-os.chdir( os.path.dirname( os.path.abspath( __file__ )))
+os.chdir( os.path.dirname( os.path.abspath( __file__ ) ) )
 
 inp = getInp()
 
 while inp != "~":
-    
-    patt = getPattern( inp)
-    rsltDex = []
+    patt = getPattern( inp )
+    rsltDex = [ ]
     rNum = 1
     
     # match input variations in new os.walk
     for pth, dct in getWalkDct().items():
-            
         # store path if match segment not in a prev. matched parent
         if re.findall( patt, pth.lower() ):
-            
             stored = False
             for rslt in rsltDex:
                 # stored True if not match any pth minus prev. results
-                if True not in [ True for el in pth.split(rslt[2]) 
+                if True not in [ True for el in pth.split( rslt[ 2 ] )
                     if re.findall( patt, el.lower() ) ]:
                     stored = True
                     break
-                    
+            
             if not stored:
-                rsltDex.append( ( rNum, None, pth ) )
-                rNum +=1
+                rsltDex.append( (rNum, None, pth) )
+                rNum += 1
         
         # store any matching files at path
-        for fil in dct['files']:
+        for fil in dct[ 'files' ]:
             if re.findall( patt, fil.lower() ):
-                rsltDex.append( ( rNum, fil, pth ) )
-                rNum +=1
+                rsltDex.append( (rNum, fil, pth) )
+                rNum += 1
     
     # output any results indexed
     for num, fil, pth in rsltDex:
         loc = f"FILE [ {fil} ]" if fil else "PATH"
         print( f"MATCH [ {num} ] IN {loc}:\n   AT [ {pth} ]" )
-        
+    
     # offer action
-    if len( rsltDex ) == 0: 
+    if len( rsltDex ) == 0:
         print( "No results" )
         inp = getInp()
     else: inp = getAction( rsltDex )
-
